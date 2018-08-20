d82c69f8592874f7f9ad6dea1c819920f5e17a17
SatoShun <shun.sato1@gmail.com>
2018-08-20 23:10:30 +0000

    update libs

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,13 +3,13 @@ object Vers {
   val min_sdk = 25
   val target_sdk = 28
 
-  val kotlin = "1.2.60"
+  val kotlin = "1.2.61"
   val couroutine = "0.24.0"
   val ktlint = "0.24.0"
 }
 
 object Libs {
-  val android_plugin = "com.android.tools.build:gradle:3.3.0-alpha05"
+  val android_plugin = "com.android.tools.build:gradle:3.3.0-alpha06"
   val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Vers.kotlin}"
   val dokka_plugin = "org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.16"
   val safeargs_plugin = "android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-alpha04"
