c30610c3119162a48b9ddc722d83ed0b5c59d66b
SatoShun <shun.sato1@gmail.com>
2018-08-22 09:50:39 +0000

    reconsider coroutine query runner

--- contentpager/src/main/java/com/github/satoshun/example/androidx/contentpager/CoroutineQueryRunner.kt
+++ contentpager/src/main/java/com/github/satoshun/example/androidx/contentpager/CoroutineQueryRunner.kt
@@ -1,26 +1,23 @@
 package com.github.satoshun.example.androidx.contentpager
 
-import android.database.Cursor
 import androidx.contentpager.content.ContentPager
 import androidx.contentpager.content.Query
 import kotlinx.coroutines.experimental.Job
 import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.launch
-import kotlin.coroutines.experimental.suspendCoroutine
 
 class CoroutineQueryRunner : ContentPager.QueryRunner {
   private val mgr = mutableMapOf<Int, Job>()
 
   override fun query(query: Query, callback: ContentPager.QueryRunner.Callback) {
     mgr[query.id] = launch(UI) {
-      callback.onQueryFinished(query, query.load(callback))
+      callback.onQueryFinished(query, query.load(callback).await())
     }
   }
 
-  private suspend fun Query.load(callback: ContentPager.QueryRunner.Callback) =
-      suspendCoroutine<Cursor> {
-        it.resume(callback.runQueryInBackground(this)!!)
-      }
+  private fun Query.load(callback: ContentPager.QueryRunner.Callback) =
+      async { callback.runQueryInBackground(this@load)!! }
 
   override fun isRunning(query: Query): Boolean {
     return mgr[query.id]?.isActive == true
