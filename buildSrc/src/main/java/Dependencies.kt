98795f2ec5ce8e84cc7458ae458c3e3e955bc83d
SatoShun <shun.sato1@gmail.com>
2018-09-02 23:14:22 +0000

    room

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -9,7 +9,7 @@ object Vers {
 }
 
 object Libs {
-  val android_plugin = "com.android.tools.build:gradle:3.2.0-rc02"
+  val android_plugin = "com.android.tools.build:gradle:3.1.4"
   val kotlin_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Vers.kotlin}"
   val dokka_plugin = "org.jetbrains.dokka:dokka-android-gradle-plugin:0.9.16"
   val safeargs_plugin = "android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-alpha04"
@@ -35,6 +35,10 @@ object Libs {
   val browser = "androidx.browser:browser:1.0.0-rc02"
   val navigation = "android.arch.navigation:navigation-fragment-ktx:1.0.0-alpha05"
   val navigation_ui = "android.arch.navigation:navigation-ui-ktx:1.0.0-alpha05"
+
+  val room_common = "androidx.room:room-common:2.0.0-rc01"
+  val room_runtime = "androidx.room:room-runtime:2.0.0-rc01"
+  val room_compiler = "androidx.room:room-compiler:2.0.0-rc01"
 
   val rxwebview = "com.github.satoshun.RxWebView:rxwebview-kotlin:2.2.0"
 
---
+++ room/src/main/java/com/github/satoshun/example/androidx/room/RoomMainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.satoshun.example.androidx.room
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RoomMainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
