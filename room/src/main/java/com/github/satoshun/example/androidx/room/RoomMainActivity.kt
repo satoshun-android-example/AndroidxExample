47d476d063dbc3294cda56912d0fc0e4e5cd6cfd
SatoShun <shun.sato1@gmail.com>
2018-10-21 04:58:58 +0000

    implements paging

--- room/src/main/java/com/github/satoshun/example/androidx/room/RoomMainActivity.kt
+++ room/src/main/java/com/github/satoshun/example/androidx/room/RoomMainActivity.kt
@@ -1,19 +1,64 @@
 package com.github.satoshun.example.androidx.room
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.lifecycle.Observer
 import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
+import com.github.satoshun.example.common.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class RoomMainActivity : AppCompatActivity() {
+class RoomMainActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
     val database = Room
-        .databaseBuilder(this, MyDatabase::class.java, "database")
+        .databaseBuilder(this , MyDatabase::class.java, "database")
         .build()
 
-    val list = LivePagedListBuilder(database.author().getAuthors(), 10).build()
+    val adapter = MainAdapter(MainItemCallback())
+    with(recycler) {
+      this.adapter = adapter
+      this.layoutManager = LinearLayoutManager(this@RoomMainActivity)
+    }
+    val authors = LivePagedListBuilder(database.author().getAuthors(), 10)
+        .build()
+    authors.observe(this, Observer {
+      adapter.submitList(it!!)
+    })
+  }
+}
+
+private class MainAdapter(
+  diffCallback: DiffUtil.ItemCallback<Author>
+) : PagedListAdapter<Author, MainViewHolder>(diffCallback) {
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+    return MainViewHolder(ImageView(parent.context))
+  }
+
+  override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+    val user = getItem(position)!!
+  }
+}
+
+private class MainViewHolder(
+  itemView: View
+) : RecyclerView.ViewHolder(itemView)
+
+private class MainItemCallback : DiffUtil.ItemCallback<Author>() {
+  override fun areItemsTheSame(oldItem: Author, newItem: Author): Boolean {
+    return oldItem == newItem
+  }
+
+  override fun areContentsTheSame(oldItem: Author, newItem: Author): Boolean {
+    return oldItem == newItem
   }
 }
