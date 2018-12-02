80b3bc80c2e59273ea7215e1ae862dd899a69dfb
SatoShun <shun.sato1@gmail.com>
2018-12-02 03:03:09 +0000

    rename

--- constraintlayout/src/main/java/com/github/satoshun/example/androidx/constraintlayout/ConstraintLayoutActivity.kt
+++ constraintlayout/src/main/java/com/github/satoshun/example/androidx/constraintlayout/ConstraintLayoutActivity.kt
@@ -3,7 +3,7 @@ package com.github.satoshun.example.androidx.constraintlayout
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class ConstraintLayoutActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
