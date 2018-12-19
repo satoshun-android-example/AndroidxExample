37f012b125b3ba419acca6a2b1067c39e23f869e
SatoShun <shun.sato1@gmail.com>
2018-12-19 07:01:18 +0000

    remove unencessary

--- savedstate/src/main/java/com/github/satoshun/example/androidx/savedstate/SavedstateActivity.kt
+++ savedstate/src/main/java/com/github/satoshun/example/androidx/savedstate/SavedstateActivity.kt
@@ -7,5 +7,7 @@ class SavedstateActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    val registry = getBundleSavedStateRegistry()
   }
 }
