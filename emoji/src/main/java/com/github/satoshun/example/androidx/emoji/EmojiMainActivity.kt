3af7978a383d82a1db71e2dcb305e2a07ab7dd65
SatoShun <shun.sato1@gmail.com>
2019-05-30 05:51:42 +0000

    r

--- emoji/src/main/java/com/github/satoshun/example/androidx/emoji/EmojiMainActivity.kt
+++ emoji/src/main/java/com/github/satoshun/example/androidx/emoji/EmojiMainActivity.kt
@@ -3,9 +3,9 @@ package com.github.satoshun.example.androidx.emoji
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class EmojiMainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    setContentView(R.layout.emoji_main_act)
   }
 }
